f5fd994bee3ea721f2c129a818b2fcdd7134e869
AbreuRico <160977095+AbreuRico@users.noreply.github.com>
2025-03-18 11:08:08 -0300

    Update page.tsx
    Contact alterado!

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,13 +9,13 @@ export default function Contact() {
         <h1 className="text-3xl font-bold">Contato</h1>
         <p className="text-lg text-gray-600 mt-4">Me encontre nas redes sociais!</p>
         <div className="flex gap-6 mt-6">
-          <a href="https://github.com/seu-usuario" target="_blank">
+          <a href="https://github.com/AbreuRico" target="_blank">
             <FaGithub className="text-4xl hover:text-gray-500 transition" />
           </a>
-          <a href="https://linkedin.com/in/seu-usuario" target="_blank">
+          <a href="https://www.linkedin.com/in/guilherme-abreu-barbosa-302b7916a/" target="_blank">
             <FaLinkedin className="text-4xl hover:text-blue-500 transition" />
           </a>
-          <a href="https://instagram.com/seu-usuario" target="_blank">
+          <a href="https://www.instagram.com/abreugagliasso/" target="_blank">
             <FaInstagram className="text-4xl hover:text-pink-500 transition" />
           </a>
         </div>
